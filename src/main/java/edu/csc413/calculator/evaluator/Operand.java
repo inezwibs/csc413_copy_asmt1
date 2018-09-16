ba58de677496ebc2526ae5096d773c99c4894a5e
inezwibs <inezwibs@users.noreply.github.com>
2018-09-16 14:31:15 -0700

    check method update
    fixed it with try catch instead of regex

--- src/main/java/edu/csc413/calculator/evaluator/Operand.java
+++ src/main/java/edu/csc413/calculator/evaluator/Operand.java
@@ -1,24 +1,28 @@
-package edu.csc413.calculator.evaluator;
-import java.lang.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+package com.company;
+
+import static java.lang.Integer.getInteger;
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
+
 
 /**
  * Operand class used to represent an operand
  * in a valid mathmetical expression.
  */
 public class Operand {
+
   int value;
   /**
   * construct operand from string token.
-  */  
-  public Operand( String token ) {
-      value = Integer.parseInt(token);
+  */
+  public Operand (String token){
+    this.value = Integer.parseInt(token);
+
   }
   /**
    * construct operand from integer
    */
-  public Operand( int value ) {
+  public Operand(int value ) {
 
     this.value = value;
   }
@@ -28,20 +32,19 @@ public class Operand {
   public int getValue() {
 
     return value;
-    //changing this return 0 to return value
   }
   /**
    * Check to see if given token is a valid
    * operand.
    */
-  public static boolean check( String token ) { //valid when token is an Operand  1,2,3,etc.
-    String regex = "[+-]?[0-9][0-9]*";//using regext to check for integer in string
+  public static boolean check( String token ) {
 
-    if (token.contains(regex)){
-
+    try {
+      int num = Integer.parseInt(token);
       return true;
+    } catch (NumberFormatException e) {
+      return false;
     }
 
-    return false;
   }
 }
